b6e24d465346afcad9040f957e81249a1091151d
hwang <gxd0214@gmail.com>
2024-01-03 18:46:26 +0900

    엔티티 매핑 객체와 테이블 매핑, JPA 스키마 자동 생성, 엔티티 어노테이션

--- JPA_Basic/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ JPA_Basic/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,9 @@
 package hellojpa;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args){
